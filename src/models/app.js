cfd30b1f2c3a04cc87a7e50797da5cbe2530c5b9
Peranut W <peacher5@hotmail.com>
2020-04-22 15:34:26 +0700

    Reformat codes

--- src/models/app.js
+++ src/models/app.js
@@ -1,28 +1,28 @@
-const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 export default {
   state: {
     fname: '',
-    lname: '',
+    lname: ''
   },
   reducers: {
     setFname(state, fname) {
       return {
         ...state,
-        fname,
+        fname
       }
     },
     setLname(state, lname) {
       return {
         ...state,
-        lname,
+        lname
       }
-    },
+    }
   },
   effects: {
     async fetchFname() {
       await wait(4000)
       this.setFname('Hello')
-    },
-  },
+    }
+  }
 }
